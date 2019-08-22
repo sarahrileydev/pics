c26565d222ef0aa6bf4c2eb85f7ae1ecb3f244d2
sriley <sarahriley.dev@gmail.com>
2019-08-22 18:57:30 -0400

    setting up image list file

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -1,3 +1,9 @@
 import axios from "axios";
 
-
+export default axios.create({
+  baseURL: "https://api.unsplash.com",
+  headers: {
+    Authorization:
+      "Client-ID 5698d459ed7883b73f1bbe8b4fd12407b2215a27c174f3f5bc5668caa5bca9ff"
+  }
+});
--- src/components/App.js
+++ src/components/App.js
@@ -1,28 +1,25 @@
 import React from "react";
-import axios from "axios";
 
+import unsplash from "../api/unsplash";
 import SearchBar from "./SearchBar";
+import ImageList from "./ImageList";
 
 class App extends React.Component {
   state = {
     images: []
   };
-  onSearchSubmit = async (term) => {
-    const response = await axios.get("https://api.unsplash.com/search/photos", {
-      params: { query: term },
-      headers: {
-        Authorization:
-          "Client-ID 5698d459ed7883b73f1bbe8b4fd12407b2215a27c174f3f5bc5668caa5bca9ff"
-      }
+  onSearchSubmit = async term => {
+    const response = await unsplash.get("/search/photos", {
+      params: { query: term }
     });
     this.setState({ images: response.data.results });
-  }
+  };
 
   render() {
     return (
       <div className="ui container" style={{ marginTop: "10px" }}>
         <SearchBar onSubmit={this.onSearchSubmit} />
-        Found: {this.state.images.length} images
+        <ImageList images={this.state.images} />
       </div>
     );
   }
