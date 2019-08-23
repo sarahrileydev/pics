45047810500f591daa15df5f418b4421e602c423
sriley <sarahriley.dev@gmail.com>
2019-08-23 08:45:54 -0400

    css grid, setSpans callback

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,15 +1,30 @@
 import React from "react";
 
 class ImageCard extends React.Component {
-  render() {
+  constructor(props) {
+    super(props);
+    this.state = { spans: 0 };
 
-    const {description, urls } = this.props.image;
+    //create a reference and assign it a variable so that we can refer back to it and it will tell us about the DOM node it is referencing (using to get picture height)
+    this.imageRef = React.createRef();
+  }
+
+  componentDidMount() {
+    this.imageRef.current.addEventListener("load", this.setSpans);
+  }
+
+  setSpans = () => {
+    const height = this.imageRef.current.clientHeight;
+    const spans = Math.ceil(height / 10);
+
+    this.setState({ spans });
+  };
+
+  render() {
+    const { description, urls } = this.props.image;
     return (
-      <div>
-        <img
-          alt={description}
-          src={urls.regular}
-        />
+      <div style={{ gridRowEnd: `span ${this.state.spans}` }}>
+        <img ref={this.imageRef} alt={description} src={urls.regular} />
       </div>
     );
   }
